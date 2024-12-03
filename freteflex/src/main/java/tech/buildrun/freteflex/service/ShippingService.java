97ce150be534c2f8ecc0bc030642ca4aff5095df
Carlos Henrique <ichfs9108@gmail.com>
2024-12-02 23:51:53 -0300

    feat: Criando a Service de calculo de frete

---
+++ freteflex/src/main/java/tech/buildrun/freteflex/service/ShippingService.java
@@ -0,0 +1,14 @@
+package tech.buildrun.freteflex.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public Double calculate(String shippingType,
+                            Double distance,
+                            Double weight) {
+        return 0.0;
+    }
+
+}
